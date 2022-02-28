0ba2f9f8ac7a917253afa25f11ac986b4e0858e7
Fishdestroyer <jeffwhitner@gmail.com>
2022-02-28 17:40:37 -0600

    Delete this commit, restructuring code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'); 
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/gp3-consulting',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/grpup-project-3-gp3-consulting',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
