3c54469c35a8392d2dcaaa37887dfb34ebdcbd20
Fishdestroyer <jeffwhitner@gmail.com>
2022-03-01 16:43:59 -0600

    Delete this commit

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-primary mb-4 py-2 flex-row align-center">
+            <div className="container flex-row justify-space-between-lg justify-center align-center">
+                <h1>GP3 Consulting</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
