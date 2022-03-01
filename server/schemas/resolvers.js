55a963cbbb7da09e1b5c5539ce94555f4e43efad
Fishdestroyer <jeffwhitner@gmail.com>
2022-02-28 19:22:03 -0600

    Updated server functions and back end

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,94 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({_id: context.user._id})
+                .select('-__v -password')
+                .populate('reviews');
+
+                return userData;
+            }
+            throw new AuthenticationError('Sign in first!')
+        },
+        users: async () => {
+            return User.find()
+            .select('-__ -password')
+            .populate('reviews')
+            .populate('reactions');
+
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('reviews')
+            .populate('reactions');
+        },
+        reviews: async( parent, { username }) => {
+            const params = username ? { username } : {};
+            return Reviews.find(params).sort({ createdAt: -1 });
+
+        },
+        review: async (parent, { _id }) => {
+            return Reviews.findOne({ _id });
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            console.log('In add user');
+            const user = await User.crreate(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Wrong Shit Bitch!');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('C-mon man, get your shit together!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addReviews: async (parent, args, context) => {
+            if (context.user) {
+                const reviews = await Reviews.create({...args, username: context.user.username});
+
+                await User.findByIdAndUpdate (
+                    {_id: context.user._id},
+                    { $push: { reviews: review._id } },
+                    { new: true }
+                );
+                return reviews;
+            }
+            throw new AuthenticationError('No no no, you gotta log in!');
+        },
+
+        addReaction: async (parent, { reviewId, reactionBody }, context) => {
+            if(context.user) {
+                const updatedReviews = await Reviews.findOneAndUpdate(
+                    { _id: reviewId },
+                    { $push: { reactions: { reactionBody, username: context.user.username } } },
+                    { new: true, runValidators: true}
+                );
+
+                return updatedReviews;
+            }
+            throw new AuthenticationError('Again with the not logging in?!?!');
+        },
+
+        
+    }
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+typer User {
+    _id: ID
+    username: String
+    email: String
+    reviews: int
+
+}
+
+type Reviews {
+    _id: ID
+    reviewText: String
+    createdAt: String
+    username: String
+    reactions: [Reaction]
+}
+
+type Reaction {
+_id: ID
+reactionBody: String
+createdAt: String
+username: String
+}
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    reviews(username: String!): [Reviews]
+    review(_id: ID!): Reviews
+}
+
+type Mutation {
+    login(email: String!, password: String!,): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addReviews(reviewsId: ID!, reactionBody: String!): Reviews
+    addReaction(reviewsId: ID!, reactionBody: String!): Reviews
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`;
+
+module.exports = typeDefs;
