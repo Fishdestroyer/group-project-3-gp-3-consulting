83e8760fcc6ea401e3f42f57cf08739a5002f846
FrankShock <schockeyf@gmail.com>
2022-03-01 00:08:05 -0600

    added reviews model

---
+++ server/models/Reviews.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const reviewsSchema = new Schema(
+  {
+    reviewText: {
+      type: String,
+      required: "You need to leave a Review!",
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Reviews = model("Reviews", reviewsSchema);
+
+module.exports = Reviews;
