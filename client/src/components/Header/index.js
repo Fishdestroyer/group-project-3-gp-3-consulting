07c7745c76280e444463a64fdfca08317bb84f87
Fishdestroyer <jeffwhitner@gmail.com>
2022-03-01 21:44:28 -0600

    test this out

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,8 +4,20 @@ const Header = () => {
     return (
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
             <div className="container flex-row justify-space-between-lg justify-center align-center">
-                <h1>GP3 Consulting</h1>
+            <h1><strong>COW Money Making</strong></h1>
+                <div class="tab-1">
                 
+                <button class="tablinks" onclick="">Contact Us</button>
+                <button class="tablinks" onclick="">sign up</button>
+                <button class="tablinks" onclick="">Customer reviews</button>
+           
+            <div class="tab-2">
+                <button class="tablinks" onclick="">Why choose us</button>
+                <button class="tablinks" onclick="">Meet the team</button>
+                <button class="tablinks" onclick="">Boohoo</button>
+            </div>
+            </div>
+            <h3><strong>Let us help your business grow!</strong></h3>
             </div>
         </header>
     );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-//import { useQuery } from '@apollo/client';
-//import { QUERY_THOUGHTS } from '../utils/queries';
+import { useQuery } from '@apollo/client';
+import { QUERY_REVIEWS } from '../utils/queries';
 //import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
